29ebce7bcb6ec9dfa7965e9ca1c6ca88ea314b86
Danilo Ancilotto <daniloancilotto@gmail.com>
2021-06-01 19:18:50 -0300

    Updated 4kvideodownloader app version

--- ubuntu.sh
+++ ubuntu.sh
@@ -217,7 +217,7 @@ printLine "4K Video Downloader"
 root_app_name="4kvideodownloader"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_version="4.15.1"
+root_app_version="4.16.2"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
