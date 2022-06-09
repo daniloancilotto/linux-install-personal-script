3dd90d5a2a407a59bf548b65117a701f13825878
Danilo Ancilotto <daniloancilotto@gmail.com>
2022-06-09 16:48:38 -0300

    Updated zoiper5 app version

--- README.md
+++ README.md
@@ -86,7 +86,7 @@ curl -H 'Cache-Control: no-cache' -sSL https://raw.githubusercontent.com/daniloa
     - ~/.local/share/applications/ventoy.desktop
 - Virt-Manager - Latest (Repository)
 - VLC - Lastest (Repository)
-- [Zoiper5 - 5.5.10 (Dpkg)](https://www.zoiper.com/en/voip-softphone/download/current)
+- [Zoiper5 - 5.5.12 (Dpkg)](https://www.zoiper.com/en/voip-softphone/download/current)
   - Autostart
     - ~/.config/autostart/Zoiper5.desktop
 - Language Pack Pt - Latest (Repository)
--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "LINUX PERSONAL SCRIPT (UBUNTU)"
-echo "Version: 2022.5.31-930"
+echo "Version: 2022.6.9-1650"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -484,8 +484,8 @@ printLine "Zoiper5"
 root_app_name="zoiper5"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_dropbox_path="zxiilfwh569xc5p"
-root_app_version="5.5.10"
+root_app_dropbox_path="cj4x0bvlc6mr8gt"
+root_app_version="5.5.12"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
