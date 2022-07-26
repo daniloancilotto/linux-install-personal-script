ab16cb09e1177c5b41717b912ad0f39783e6d92f
Danilo Ancilotto <daniloancilotto@gmail.com>
2022-07-26 12:02:57 -0300

    Updated zoiper app version

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "LINUX PERSONAL SCRIPT (UBUNTU)"
-echo "Version: 2022.7.15-1940"
+echo "Version: 2022.7.26-1200"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -70,7 +70,6 @@ printLine "Update"
 sudo apt update
 
 printLine "Kernel"
-
 sudo apt-mark auto $(apt-mark showmanual | grep -E "^linux-([[:alpha:]]+-)+[[:digit:].]+-[^-]+(|-.+)$")
 
 swappiness="10"
@@ -486,8 +485,8 @@ printLine "Zoiper5"
 root_app_name="zoiper5"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_dropbox_path="cj4x0bvlc6mr8gt"
-root_app_version="5.5.12"
+root_app_dropbox_path="ets3ozo6gq2443t"
+root_app_version="5.5.13"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
