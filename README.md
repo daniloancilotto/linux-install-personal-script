260e76d42660b9fe5690a8136aafc0c7970a7fa6
Danilo Ancilotto <daniloancilotto@gmail.com>
2022-10-07 14:51:04 -0300

    Updated 4kvideodownloader app version

--- README.md
+++ README.md
@@ -52,7 +52,7 @@ curl -H 'Cache-Control: no-cache' -sSL https://raw.githubusercontent.com/daniloa
   - Menu
     - ~/.local/share/applications/htop.desktop
 - Neofetch - Latest (Repository)
-- [4K Video Downloader - 4.21.5 (Dpkg)](https://www.4kdownload.com/downloads)
+- [4K Video Downloader - 4.21.7 (Dpkg)](https://www.4kdownload.com/downloads)
 - [Angry IP Scanner - 3.8.2 (Dpkg)](https://angryip.org/download/)
 - [Arduino IDE - 2.0.0 (AppImage)](https://www.arduino.cc/en/software)
   - Menu
--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "LINUX PERSONAL SCRIPT (UBUNTU)"
-echo "Version: 2022.9.22-2030"
+echo "Version: 2022.10.7-1450"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -159,7 +159,7 @@ printLine "4K Video Downloader"
 root_app_name="4kvideodownloader"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_version="4.21.5"
+root_app_version="4.21.7"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
