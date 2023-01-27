605ee5ec53bb2091e0155c62163a2e646b7837ff
Danilo Ancilotto <daniloancilotto@gmail.com>
2023-01-27 12:28:27 -0300

    Updated balenaetcher app version

--- README.md
+++ README.md
@@ -62,7 +62,7 @@ curl -H 'Cache-Control: no-cache' -sSL https://raw.githubusercontent.com/daniloa
 - Audacity - Latest (Repository)
   - Menu
     - ~/.local/share/applications/audacity.desktop
-- [balenaEtcher - 1.13.3 (AppImage)](https://www.balena.io/etcher/)
+- [balenaEtcher - 1.14.3 (AppImage)](https://www.balena.io/etcher/)
   - Menu
     - ~/.local/share/applications/appimagekit-balena-etcher-electron.desktop
 - [Dropbox - 2020.03.04 (Dpkg)](https://www.dropbox.com/install)
--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "LINUX PERSONAL SCRIPT (UBUNTU)"
-echo "Version: 2023.1.25-1950"
+echo "Version: 2023.1.27-1230"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -271,7 +271,7 @@ printLine "balenaEtcher"
 home_app_name="balena-etcher"
 home_app_subdir="$home_app_dir/$home_app_name"
 home_app_cversion="`cat "$home_app_subdir/version.txt"`"
-home_app_version="1.13.3"
+home_app_version="1.14.3"
 
 if [ "$home_app_cversion" != "$home_app_version" ]
 then
