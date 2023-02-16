4bb717a22a6a45178288f91bfb020e563eb19d14
Danilo Ancilotto <daniloancilotto@gmail.com>
2023-02-16 14:34:32 -0300

    Updated angryipscanner app version

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ curl -H 'Cache-Control: no-cache' -sSL https://raw.githubusercontent.com/daniloa
     - ~/.local/share/applications/htop.desktop
 - Neofetch - Latest (Repository)
 - [4K Video Downloader - 4.23.1 (Dpkg)](https://www.4kdownload.com/downloads)
-- [Angry IP Scanner - 3.9.0 (Dpkg)](https://angryip.org/download/)
+- [Angry IP Scanner - 3.9.1 (Dpkg)](https://angryip.org/download/)
 - [Arduino IDE - 2.0.3 (AppImage)](https://www.arduino.cc/en/software)
   - Menu
     - ~/.local/share/applications/arduino-ide.desktop
--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "LINUX PERSONAL SCRIPT (UBUNTU)"
-echo "Version: 2023.2.8-1720"
+echo "Version: 2023.2.16-1440"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -187,7 +187,7 @@ printLine "Angry IP Scanner"
 root_app_name="angryipscanner"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_version="3.9.0"
+root_app_version="3.9.1"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
