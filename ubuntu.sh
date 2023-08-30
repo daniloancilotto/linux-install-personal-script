63bd3072eed10e9fca0bbc039b75f3695ac8ceaa
Danilo Ancilotto <daniloancilotto@gmail.com>
2023-08-30 19:16:40 -0300

    Updated arduino ide app version

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "LINUX PERSONAL SCRIPT (UBUNTU)"
-echo "Version: 2023.8.23-2150"
+echo "Version: 2023.8.30-1920"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -218,7 +218,7 @@ printLine "Arduino IDE"
 home_app_name="arduino-ide"
 home_app_subdir="$home_app_dir/$home_app_name"
 home_app_cversion="`cat "$home_app_subdir/version.txt"`"
-home_app_version="2.1.1"
+home_app_version="2.2.0"
 
 if [ "$home_app_cversion" != "$home_app_version" ]
 then
