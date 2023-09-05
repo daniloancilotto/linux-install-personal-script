5416f6feeca5640f4a1a17cffa386b130f77d34e
Danilo Ancilotto <daniloancilotto@gmail.com>
2023-09-05 15:16:56 -0300

    Updated 4kvideodownloader and arduinoide app versions

--- README.md
+++ README.md
@@ -53,9 +53,9 @@ curl -H 'Cache-Control: no-cache' -sSL https://raw.githubusercontent.com/daniloa
   - Menu
     - ~/.local/share/applications/htop.desktop
 - Neofetch - Latest (Repository)
-- [4K Video Downloader - 4.26.1 (Dpkg)](https://www.4kdownload.com/downloads)
+- [4K Video Downloader - 4.27.0 (Dpkg)](https://www.4kdownload.com/downloads)
 - [Angry IP Scanner - 3.9.1 (Dpkg)](https://angryip.org/download/)
-- [Arduino IDE - 2.2.0 (AppImage)](https://www.arduino.cc/en/software)
+- [Arduino IDE - 2.2.1 (AppImage)](https://www.arduino.cc/en/software)
   - Menu
     - ~/.local/share/applications/arduino-ide.desktop
   - User Groups
--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "LINUX PERSONAL SCRIPT (UBUNTU)"
-echo "Version: 2023.8.30-1920"
+echo "Version: 2023.9.5-1520"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -162,7 +162,7 @@ printLine "4K Video Downloader"
 root_app_name="4kvideodownloader"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_version="4.26.1"
+root_app_version="4.27.0"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
@@ -218,7 +218,7 @@ printLine "Arduino IDE"
 home_app_name="arduino-ide"
 home_app_subdir="$home_app_dir/$home_app_name"
 home_app_cversion="`cat "$home_app_subdir/version.txt"`"
-home_app_version="2.2.0"
+home_app_version="2.2.1"
 
 if [ "$home_app_cversion" != "$home_app_version" ]
 then
