98be73f162f8997e95540ce1c152c407e74433eb
Danilo Ancilotto <daniloancilotto@gmail.com>
2023-10-17 21:56:25 -0300

    Updated ventoy app version

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "LINUX PERSONAL SCRIPT (UBUNTU)"
-echo "Version: 2023.9.19-2120"
+echo "Version: 2023.10.17-2200"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -438,7 +438,7 @@ printLine "Ventoy"
 home_app_name="ventoy"
 home_app_subdir="$home_app_dir/$home_app_name"
 home_app_cversion="`cat "$home_app_subdir/version.txt"`"
-home_app_version="1.0.95"
+home_app_version="1.0.96"
 
 if [ "$home_app_cversion" != "$home_app_version" ]
 then
